5e8f7ad7cffe2929311da67879beca3e2e530bd9
gizmrkv <gizmrkv@gmail.com>
2024-07-20 15:28:15 +0900

    Fix: LightGBM

--- src/polars_pipeline/model/lightgbm_model.py
+++ src/polars_pipeline/model/lightgbm_model.py
@@ -27,7 +27,7 @@ class LightGBM(Transformer):
         self.predict_fn = predict_fn or (lambda booster, X: booster.predict(X))  # type: ignore
         self.booster: lgb.Booster | None = None
         self.X_columns: List[str] | None = None
-        self.y_columns: List[str] | None = None
+        self.y_column: str | None = None
 
     def fit(self, X: FrameType, y: FrameType | None = None):
         if isinstance(X, LazyFrame) or isinstance(y, LazyFrame):
@@ -36,8 +36,11 @@ class LightGBM(Transformer):
         if y is None:
             raise ValueError("y cannot be None")
 
+        if len(y.columns) > 1:
+            raise ValueError("y should have only one column")
+
         self.X_columns = X.columns
-        self.y_columns = y.columns
+        self.y_column = y.columns[0]
 
         X_np = X.to_numpy().squeeze()
         y_np = y.to_numpy().squeeze()
@@ -51,7 +54,7 @@ class LightGBM(Transformer):
                 self.__class__.__name__, self.transform.__name__
             )
 
-        if self.booster is None or self.X_columns is None or self.y_columns is None:
+        if self.booster is None or self.X_columns is None or self.y_column is None:
             raise NotFittedError(self.__class__.__name__)
 
         if set(self.X_columns) != set(X.columns):
@@ -62,4 +65,10 @@ class LightGBM(Transformer):
         X_np = X.to_numpy().squeeze()
         pred = self.predict_fn(self.booster, X_np)
         assert isinstance(pred, np.ndarray)
-        return pl.from_numpy(pred, schema=self.y_columns)
+
+        if self.params["objective"] == "multiclass":
+            return pl.from_numpy(
+                pred, schema=[f"{self.y_column}_{i}" for i in range(pred.shape[1])]
+            )
+        else:
+            return pl.from_numpy(pred, schema=[self.y_column])
