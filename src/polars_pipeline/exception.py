44057a971eecd827d0b7c1f44f6664199a9149ce
gizmrkv <gizmrkv@gmail.com>
2024-07-20 15:09:14 +0900

    Add: ColumnsMismatchError

--- src/polars_pipeline/exception.py
+++ src/polars_pipeline/exception.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class NotFittedError(Exception):
     def __init__(self, name: str):
         self.name = name
@@ -9,3 +12,14 @@ class LazyFrameNotSupportedError(Exception):
         self.name = name
         self.method = method
         super().__init__(f"LazyFrame not supported: {name}.{method}")
+
+
+class ColumnsMismatchError(Exception):
+    def __init__(self, name: str, columns: List[str], fitted_columns: List[str]):
+        self.name = name
+        self.columns = columns
+        self.fitted_columns = fitted_columns
+        msg = f"Columns of X do not match the columns of the fitted {name}.\n"
+        msg += f"Columns of X: {columns}\n"
+        msg += f"Columns of the fitted {name}: {fitted_columns}"
+        super().__init__(msg)
